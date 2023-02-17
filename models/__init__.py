5301f9aa0a33cbf0900a6a4dec5d62325875aac4
alimohammadiamirhossein <alimohammadiamirhossein@gmail.com>
2023-02-17 17:22:03 +0330

    Finished indicators and Dataset!

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,16 @@
 from .random_forest import RandomForest
 from .sarimax import Sarimax
-# from .orbit import Orbit
+from .orbit import Orbit
 from .LSTM import MyLSTM
 from .GRU import MyGRU
 from .arima import MyARIMA
-# from .prophet import MyProphet
+from .prophet import MyProphet
 
 MODELS = {'random_forest': RandomForest,
           'sarimax': Sarimax,
-          # 'orbit': Orbit,
+          'orbit': Orbit,
           'lstm': MyLSTM,
           'gru': MyGRU,
           'arima': MyARIMA,
-          # 'prophet': MyProphet
+          'prophet': MyProphet
           }
