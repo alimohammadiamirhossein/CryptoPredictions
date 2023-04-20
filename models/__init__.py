5f9dfe661e21f352fb55ce92f5365b341e0ac172
alimohammadiamirhossein <alimohammadiamirhossein@gmail.com>
2023-04-20 20:57:16 +0330

    remove api directory for better user experience

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,19 @@
 from .random_forest import RandomForest
 from .sarimax import Sarimax
-# from .orbit import Orbit
+from .orbit import Orbit
 from .LSTM import MyLSTM
 from .GRU import MyGRU
 from .arima import MyARIMA
-# from .prophet import MyProphet
+from .prophet import MyProphet
 from .xgboost import MyXGboost
 
 
 MODELS = {'random_forest': RandomForest,
           'sarimax': Sarimax,
-          # 'orbit': Orbit,
+          'orbit': Orbit,
           'lstm': MyLSTM,
           'gru': MyGRU,
           'arima': MyARIMA,
-          # 'prophet': MyProphet,
+          'prophet': MyProphet,
           'xgboost': MyXGboost
           }
